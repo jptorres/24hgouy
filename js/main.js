53cff9228316f676052b186adf621116e08b07ba
JP <torres.jp86@gmail.com>
2014-05-02 18:23:50 +0200

    Animation header

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
+/*####################*/
+   /*Cloud animation*/
+/*####################*/
+var widthWindow = $(window).width();
 
+$(document).ready(function(){
+	setTimeout("cloudAnimation()", 1000);
+});
+
+function cloudAnimation(){
+	cloud1();
+	/*cloud2();
+	cloud3();*/
+}
+
+function cloud1(){
+	$("#cloud1").animate({left: widthWindow}, 10000).animate({left:"-432px"}, 0);
+	setTimeout("cloudAnimation()", 10000);
+}
+/*function cloud2(){
+	$("#cloud2").animate({left: widthWindow}, 20000).animate({left:"-410px"}, 0);
+	setTimeout("cloudAnimation()", 15000);
+}function cloud3(){
+	$("#cloud3").animate({left: widthWindow}, 300000).animate({left:"-485px"}, 0);
+	setTimeout("cloudAnimation()", 1000);
+}*/
