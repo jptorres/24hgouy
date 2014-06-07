26f5c309e600b0b20de7113c1327c04a55b57d38
jetorres <jetorres@M20140009.xperthis.local>
2014-06-08 01:51:16 +0200

    header, content, countdown and facebook.

--- js/main.js
+++ js/main.js
@@ -15,3 +15,58 @@ function cloud1(){
 	$("#cloud1").animate({left: widthWindow}, 30000).animate({left:"-350px"}, 0);
 	setTimeout("cloudAnimation()", 1000);
 }
+
+/*####################*/
+   /*countDown*/
+/*####################*/
+$("#daysCountdown").TimeCircles({
+	animation: "smooth",
+	circle_bg_color: "#3B1E7C",
+	fg_width: 0.06,
+	time: {
+	    Days: {
+	    	text: "J", 
+			color: "#A6D30B"
+		},
+	    Hours: {
+	    	text: "H",
+	    	color: "#A6D30B",
+			show:false
+	    },
+	    Minutes: {
+	    	text: "min",
+	    	color: "#A6D30B",
+	    	show:false
+	    },
+	    Seconds: {
+	    	text: "sec",
+	    	color: "#A6D30B",
+	    	show:false
+	    }
+	}
+});
+$("#hoursCountdown").TimeCircles({
+	animation: "smooth",
+	circle_bg_color: "#3B1E7C",
+	fg_width: 0.06,
+	time: {
+	    Days: {
+	    	text: "J", 
+			color: "#A6D30B",
+			show:false
+		},
+	    Hours: {
+	    	text: "H",
+	    	color: "#A6D30B"
+	    },
+	    Minutes: {
+	    	text: "min",
+	    	color: "#A6D30B"
+	    	
+	    },
+	    Seconds: {
+	    	text: "sec",
+	    	color: "#A6D30B"
+	    }
+	}
+});
